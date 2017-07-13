21c565ad6af81e20e8b0a9dc3f47d30260e52d0e
nothing <nocoding@163.com>
2017-07-13 14:34:59 +0800

    autolayout

---
+++ AndroidAutoLayout-master/sample/src/main/java/com/zhy/sample/fragment/RecyclerViewFragment.java
@@ -0,0 +1,75 @@
+package com.zhy.sample.fragment;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.zhy.autolayout.utils.AutoUtils;
+import com.zhy.base.adapter.recyclerview.CommonAdapter;
+import com.zhy.sample.R;
+import com.zhy.sample.view.DividerItemDecoration;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RecyclerViewFragment extends Fragment
+{
+    private View mView;
+    private RecyclerView mRecyclerView;
+    private List<String> mList;
+    private Context mContext;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState)
+    {
+        mView = inflater.inflate(R.layout.fragment_recyclerview, container, false);
+        initView();
+        return mView;
+    }
+
+    private void initView()
+    {
+        mContext = getActivity();
+        mRecyclerView = (RecyclerView) mView.findViewById(R.id.id_recyclerview);
+        mList = new ArrayList<String>();
+        for (int i = 0; i < 50; i++)
+        {
+            mList.add(i + "");
+        }
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        mRecyclerView.setAdapter(new MyAdapter(getActivity(),mList));
+
+        mRecyclerView.addItemDecoration(new DividerItemDecoration(getActivity(),
+                DividerItemDecoration.VERTICAL_LIST));
+    }
+
+    class MyAdapter extends CommonAdapter<String>
+    {
+        public MyAdapter(Context context, List<String> datas)
+        {
+            super(context, R.layout.recyclerview_item, datas);
+        }
+
+        @Override
+        public com.zhy.base.adapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType)
+        {
+            com.zhy.base.adapter.ViewHolder viewHolder = super.onCreateViewHolder(parent, viewType);
+            AutoUtils.autoSize(viewHolder.getConvertView());
+            return viewHolder;
+        }
+
+        @Override
+        public void convert(com.zhy.base.adapter.ViewHolder viewHolder, String s)
+        {
+        }
+    }
+
+
+
+}
---
+++ AndroidAutoLayout-master/widgetsample/src/main/java/com/zhy/autolayout/test/widgets/MainActivity.java
@@ -0,0 +1,73 @@
+package com.zhy.autolayout.test.widgets;
+
+import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.zhy.autolayout.test.widgets.fragments.SimpleFragment;
+import com.zhy.autolayout.widget.AutoLayoutWidgetActivity;
+
+public class MainActivity extends AutoLayoutWidgetActivity {
+    private TabLayout mTabLayout;
+    private ViewPager mViewPager;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle("标题");
+        setSupportActionBar(toolbar);
+        mTabLayout = (TabLayout) findViewById(R.id.id_tablayout);
+        mViewPager = (ViewPager) findViewById(R.id.id_viewpager);
+
+        mViewPager.setAdapter(new FragmentPagerAdapter(getSupportFragmentManager()) {
+            @Override
+            public Fragment getItem(int position) {
+                return new SimpleFragment();
+            }
+
+            @Override
+            public int getCount() {
+                return 2;
+            }
+
+            @Override
+            public CharSequence getPageTitle(int position) {
+                return "Page " + position;
+            }
+        });
+
+        mTabLayout.setupWithViewPager(mViewPager);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
