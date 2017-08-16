26f88d549e7f65ce372263190e75b24ed3784acf
nothing <nocoding@163.com>
2017-08-16 18:08:38 +0800

    mutileprocess

--- RePlugin/RepluginDemo/app/src/main/java/com/example/dengshaomin/replugindemo/MainActivity.java
+++ RePlugin/RepluginDemo/app/src/main/java/com/example/dengshaomin/replugindemo/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.dengshaomin.replugindemo;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        String s = null;
+        Log.e("1111", s.length() + "");
     }
 }
